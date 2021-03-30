d6147bcdb0d4c124c50dc17e81fc9e145e910e56
Al Ka <alkapone.public@gmail.com>
2021-03-30 10:17:36 -0300

    Quito url

--- db.js
+++ db.js
@@ -6,15 +6,3 @@ mongoose.connect(process.env.URLMONGODB, {
 }, () => {
     console.log("Database conectada")
 });
-
-
-/*
-
-mongoose.connect("mongodb+srv://root:Jzng5wqvi3yVkyjU@cluster0.rqhiv.mongodb.net/database?retryWrites=true&w=majority", {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-}, () => {
-console.log("✔ Conectado a MongoDB")
-})
-
-*/
