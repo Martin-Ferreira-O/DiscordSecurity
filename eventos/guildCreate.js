fb1fa2346c128f2dfcb5162f575df6e31a5c902c
Al Ka <alkapone.public@gmail.com>
2021-04-11 17:53:21 -0400

    Evento guildCreate

---
+++ eventos/guildCreate.js
@@ -0,0 +1,32 @@
+import pkg from "discord.js-lite";
+const { MessageEmbed } = pkg;
+export default async(client, guild) => {
+    const dueño = await client.users.fetch(guild.ownerID);
+    const embed = new MessageEmbed()
+        .setAuthor(guild.name, guild.iconURL({ dynamic: true }))
+        .setDescription("Me añadieron a un nuevo servidor, aca puedes obtener mas información al respecto")
+        .addFields([{
+            name: "Miembros",
+            value: guild.approximateMemberCount,
+            inline: true
+        }, {
+            name: "Dueño",
+            value: dueño.id + " " + dueño.tag,
+            inline: true
+        }])
+        .setColor("GREEN")
+        .setThumbnail(guild.iconURL({ dynamic: true }))
+    if (guild.me.hasPermission("VIEW_AUDIT_LOG")) {
+        const fetchedLogs = await guild.fetchAuditLogs({
+            limit: 1,
+            type: 'BOT_ADD',
+        });
+        const deletionLog = fetchedLogs.entries.first();
+        if (deletionLog) {
+            const { executor, target } = deletionLog;
+            if (target.id == client.user.id) embed.addField("Me agrego el usuario", executor.tag)
+        }
+    }
+    const channel = await client.channels.fetch("734207834866188300").catch(err => {})
+    if (channel) channel.send(embed)
+}
