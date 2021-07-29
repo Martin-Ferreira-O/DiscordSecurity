68038ee9254dad0e4a1e50c00e34d0b99892ae26
Kapone <alkapone.public@gmail.com>
2021-07-28 21:45:28 -0400

    Now the events and command support node

--- src/lib/registry.ts
+++ src/lib/registry.ts
@@ -2,14 +2,13 @@ import path from 'path';
 import { promises as fs } from 'fs';
 import Bot from '../bot';
 
-
 async function registerCommands(bot: Bot, dir = '') {
     const filePath = path.join(__dirname, dir);
     const files = await fs.readdir(filePath);
     for (const file of files) {
         const stat = await fs.lstat(path.join(filePath, file));
         if (stat.isDirectory()) registerCommands(bot, path.join(dir, file));
-        if (file.endsWith('.ts')) {
+        if (file.endsWith(".ts") || file.endsWith(".js")) {
             const Command = await import (path.join(__dirname, dir, file));
             const cmd = new Command.default();
             bot.commands.set(cmd.name, cmd);
@@ -26,18 +25,13 @@ async function registerEvents(bot: Bot, dir = '') {
     for (const file of files) {
         const stat = await fs.lstat(path.join(filePath, file));
         if (stat.isDirectory()) registerEvents(bot, path.join(dir, file));
-        if (file.endsWith('.ts')) {
-            const eventName = file.substring(0, file.indexOf(".ts"));
-            const Event = await
-            import (path.join(__dirname, dir, file));
+        if (file.endsWith(".ts") || file.endsWith(".js")) {
+            const Event = await import (path.join(__dirname, dir, file));
             const event = new Event.default();
             bot.events.set(event.name, event);
-            bot.client.on(eventName, event.run.bind(null, bot));
+            bot.client.on(event.name, event.run.bind(null, bot));
         }
     }
 }
 
-export {
-    registerCommands,
-    registerEvents
-}
+export { registerCommands, registerEvents }
